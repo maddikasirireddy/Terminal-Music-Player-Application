b67665d7711d8aeb65cacf5c1a0e0358096d59d4
Siri Reddy <maddikasirireddy@gmail.com>
2026-09-12 12:01:12 +0530

    Refactor player.js to integrate UI functions

--- player.js
+++ player.js
@@ -1,82 +1,60 @@
 const { spawn } = require("child_process");
-const fs = require("fs");
-
-// Read all the mp3 files
-const path = "./songs";
-const songs = fs.readdirSync(path).filter((el) => el.endsWith(".mp3"));
-
-console.log(`🎶 Welcome to the Songs App 🎶\n`);
-
-for (let i = 0; i < songs.length; i++) {
-  console.log(`${i + 1}: ${songs[i].split(".")[0]}`);
-}
-
-console.log(`\n🎵 Select a number to play the song`);
-
-process.stdin.setEncoding("utf-8");
-process.stdin.setRawMode(true);
+const ui = require("./ui");
 
 let childProcess = null;
 
-process.stdin.on("data", (input) => {
-  const userInput = input.toString().trim();
+function player(selected, songs) {
 
-  // Pause
-  if (userInput === "p") {
-    pause();
-  }
-
-  // Resume
-  if (userInput === "r") {
-    resume();
-  }
-
-  // Quit
-  if (userInput === "q") {
-    stop();
-    process.stdin.setRawMode(false);
-    process.exit(0);
-  }
-
-  // Select song
-  if (userInput > 0 && userInput <= songs.length) {
-    player(+userInput);
-  }
-});
-
-function player(userInput) {
-  console.log(`Selected Song: ${songs[userInput - 1]}`);
+  const song = songs[selected - 1];
 
   if (childProcess) {
     childProcess.kill("SIGKILL");
   }
 
-  childProcess = spawn("afplay", [`./songs/${songs[userInput - 1]}`]);
+  console.log(`\n▶️  Now Playing: ${song.split(".")[0]}`);
+
+  childProcess = spawn("afplay", [
+    `./songs/${song}`
+  ]);
 
   childProcess.on("close", () => {
-    console.log("Song finished...");
+    childProcess = null;
+    ui.showFinished();
   });
 }
 
 function pause() {
+
   if (childProcess) {
     childProcess.kill("SIGSTOP");
-    console.log("Song Paused...");
+    ui.showPaused();
   }
 }
 
 function resume() {
+
   if (childProcess) {
     childProcess.kill("SIGCONT");
-    console.log("Song Resumed...");
+    ui.showResumed();
   }
 }
 
 function stop() {
+
   if (childProcess) {
+
     childProcess.kill("SIGCONT");
     childProcess.kill("SIGKILL");
+
     childProcess = null;
-    console.log("Song Stopped...");
+
+    ui.showStopped();
   }
 }
+
+module.exports = {
+  player,
+  pause,
+  resume,
+  stop
+};
